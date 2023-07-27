840c13fc1560b789f1f6a8cc96face63c41f71ad
kasai <back2846@icloud.com>
2023-07-27 11:44:26 +0900

    グレーススケール画像処理

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+import cv2
+import numpy as np
+import base64
 
 app = FastAPI()
 
 origins = {
     "http://localhost:3000",
 }
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +19,21 @@ app.add_middleware(
     allow_headers= ["*"],
 )
 
-@app.get("/")
-def Hello():
-    return {"Hello":"World!"}
+def process_image(image):
+    # 画像処理のコード
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return gray_image
+
+@app.post("/upload/")
+async def upload_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    processed_image = process_image(image)
+
+    # Base64エンコードしてフロントエンドに送信
+    _, buffer = cv2.imencode('.jpg', processed_image)
+    base64_image = base64.b64encode(buffer).decode('utf-8')
+
+    return  {"processed_image": base64_image}
