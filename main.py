19979ecf7e3f6c16e989239185447cf86ad19d56
kasai <back2846@icloud.com>
2023-08-23 12:13:36 +0900

    StreamingResponseを使用してフロントエンドで画像をダウンロード可能

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from starlette.responses import JSONResponse
 import cv2
 import numpy as np
 import base64
@@ -21,12 +23,12 @@ app.add_middleware(
     allow_headers= ["*"],
 )
 
-# グレースケール化
+# グレースケール化関数
 def process_grayscale(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return gray_image
 
-# エッジ処理
+# エッジ処理関数
 def ege_processing(image, fileter_type):
     if fileter_type == "Sobel":
         ege_processed_image = cv2.Sobel(image, cv2.CV_32F, 1, 0, ksize=3)
@@ -36,57 +38,75 @@ def ege_processing(image, fileter_type):
         ege_processed_image = cv2.Canny(image, 100, 200)
     return ege_processed_image
 
-# グレーススケール化
+
+
+
+# グレーススケール化(API)
 @app.post("/gray/")
 async def upload_image(file: UploadFile = File(...)):
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    processed_image = process_grayscale(image)
+    try:
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # Base64エンコードしてフロントエンドに送信
-    _, buffer = cv2.imencode('.jpg', processed_image)
-    base64_image = base64.b64encode(buffer).decode('utf-8')
+        # 画像処理
+        processed_image = process_grayscale(image)
 
-    return  {"processed_image": base64_image}
+        retval, buffer = cv2.imencode(".jpg", processed_image)
+        image_bytes = buffer.tobytes()
 
-# エッジ処理（ラプラシアン処理）
+        def generate():
+            yield image_bytes
+
+        return StreamingResponse(generate(), media_type="image/jpeg")
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+# エッジ処理(API)
 @app.post("/edge/")
 async def upload_image(file: UploadFile = File(...), filter_type: str = Form(...)):
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    try:
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    processed_image = ege_processing(image, filter_type)
+        processed_image = ege_processing(image, filter_type)
 
-    # Base64エンコードしてフロントエンドに送信
-    _, buffer = cv2.imencode('.jpg', processed_image)
-    base64_image = base64.b64encode(buffer).decode('utf-8')
+        retval, buffer = cv2.imencode(".jpg", processed_image)
+        image_bytes = buffer.tobytes()
 
-    return  {"processed_image": base64_image}
+        def generate():
+            yield image_bytes
 
-# 電子透かし処理
+        return StreamingResponse(generate(), media_type="image/jpeg")
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+
+
+# 電子透かし処理(API)
 @app.post("/watermark/")
 async def process_data(file: UploadFile = File(...), text: str = Form(...)):
-    contents = await file.read()
+    try:
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    nparr = np.frombuffer(contents, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        processed_image = fragile.create_fragile_image(image, text)
 
-    processed_image = fragile.create_fragile_image(image, text)
-    result_s = detection.detection(processed_image)
-    print(result_s)
-    output_path = "download_files/" + file.filename
-    cv2.imwrite(output_path, processed_image)
+        retval, buffer = cv2.imencode(".png", processed_image)
+        image_bytes = buffer.tobytes()
 
-    # Base64エンコードしてフロントエンドに送信
-    _, buffer = cv2.imencode('.jpg', processed_image)
-    base64_image = base64.b64encode(buffer).decode('utf-8')
+        def generate():
+            yield image_bytes
+        
+        return StreamingResponse(generate(), media_type="image/png")
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
-    return  {"processed_image": base64_image}
 
-# 電子透かし抽出処理
+# 電子透かし抽出処理(API)
 @app.post("/detection/")
 async def upload_image(file: UploadFile = File(...)):
     contents = await file.read()
